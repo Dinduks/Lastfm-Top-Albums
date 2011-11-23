fe5f8407bd4dc37c81bd046c0f989a3244a61818
Samy Dindane <samy@dindane.com>
2011-11-23 15:53:29 +0100

    Added the charset meta to specify the homepage's encoding

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8" />
         <title>Last.fm top albums patchwork generator</title>
         <link href="main.css" rel="stylesheet" type="text/css" />
     </head>
