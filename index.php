16da3028249643fddeb62e0173a1b77004dd95e5
Dinduks <samy@dindane.com>
2012-08-22 12:17:25 +0200

    Add the GA's code

--- index.php
+++ index.php
@@ -4,6 +4,17 @@
         <meta charset="utf-8" />
         <title>Last.fm top albums patchwork generator</title>
         <link href="main.css" rel="stylesheet" type="text/css" />
+        <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-34258588-1']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+        </script>
     </head>
     <body>
         <header>Last.fm top albums patchwork generator</header>
